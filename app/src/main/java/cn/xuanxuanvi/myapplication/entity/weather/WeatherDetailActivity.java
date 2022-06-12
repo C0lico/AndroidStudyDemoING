80f9e9815a853c0e6dc6e9a90549f33d77a87dca
ChenZhouzhen <952925323@qq.com>
2022-06-12 21:31:36 +0800

    天气查询模块实体类的创建以及简单页面的设计

---
+++ app/src/main/java/cn/xuanxuanvi/myapplication/entity/weather/WeatherDetailActivity.java
@@ -0,0 +1,14 @@
+package cn.xuanxuanvi.myapplication.entity.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WeatherDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_detail);
+    }
+}
---
+++ app/src/main/java/cn/xuanxuanvi/myapplication/entity/weather/Wid.java
@@ -0,0 +1,30 @@
+/**
+  * Copyright 2022 bejson.com 
+  */
+package cn.xuanxuanvi.myapplication.entity.weather;
+
+/**
+ * Auto-generated: 2022-06-12 17:10:51
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+public class Wid {
+
+    private String day;
+    private String night;
+    public void setDay(String day) {
+         this.day = day;
+     }
+     public String getDay() {
+         return day;
+     }
+
+    public void setNight(String night) {
+         this.night = night;
+     }
+     public String getNight() {
+         return night;
+     }
+
+}
